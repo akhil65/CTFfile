4959a2a05fa4d0a96dffea4769d1186dc18305c8
Akhilesh Anand Undralla <66138208+akhil65@users.noreply.github.com>
2022-01-21 22:47:12 -0500

    Update disableMouseRightClick.js

--- disableMouseRightClick.js
+++ disableMouseRightClick.js
@@ -1,7 +1,10 @@
-function UBGenC_Run() {
-	document.addEventListener('contextmenu',
-			event => event.preventDefault());
-	el_down.innerHTML = "Right click disabled";
-			}
+jQuery(document).ready(function(){
+  jQuery(function() {
+        jQuery(this).bind("contextmenu", function(event) {
+            event.preventDefault();
+            alert('Right click disable in this site!!')
+        });
+    });
+});
   //the source reveals my favourite secret sauce 
   // DUCTF{n0_k37chup_ju57_54uc3_r4w_54uc3_9873984579843} 
